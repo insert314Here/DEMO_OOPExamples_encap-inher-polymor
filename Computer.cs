f9153c27ef077ff943876f707fefad3ecd9c091e
insert314Here <89361627+insert314Here@users.noreply.github.com>
2021-09-22 08:13:33 -0400

    Add project files.

---
+++ Computer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExamples
+{
+    class Computer
+    {
+        //public property
+        public bool isOn { get; private set; }
+
+        protected string _name = "Unknown";
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            private set //private scope allows the name value to never be changed
+            {
+                _name = value;
+            }
+        }
+
+        //creating a constructor...no return type and uses the name of the class
+        public Computer(string name) //passes an argument thru the constructor
+        {
+            this.name = name; //property value being passed into the constructor parameter
+            // this key modifier helps to distinguish the property VS the parameter
+        }
+
+        
+        //protected ensures that computer class has a scope that cannot be accessed by other methods
+        protected void TurnOn()
+        {
+            isOn = true;
+        }
+
+        protected void TurnOff()
+        {
+            isOn = false;
+        }
+
+        
+        //the togglepower method protects turnon and off so no other methods can access protected methods
+        //concept of encapsulation so certain methods are not initiated during runtime of program
+        public void TogglePower()
+        {
+            if (isOn)
+            {
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using OOPExamples;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("what is the computers name?");
+        string compName = Console.ReadLine();
+        var comp = new Computer(compName); //using constructor from 'computer.cs'
+
+        Console.WriteLine("Computer Type - " + comp.GetType());
+
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("Is computer - " + isComputer);
+        Console.WriteLine("Computer name - " + comp.name);
+
+        //these methods run sequentially so the computer is turned on then off as final before writeline
+        #region result of protected methods
+        //once the methods were PROTECTED and not PUBLIC we could not use the methods in the non-parent class
+        //comp.TurnOn();
+        //comp.TurnOff();
+
+        #endregion
+
+        comp.TogglePower(); //this call is still public which uses the PROTECTED methods
+        Console.WriteLine("Computer is on - "+comp.isOn);
+        Console.Read();
+
+    }
+}
+
